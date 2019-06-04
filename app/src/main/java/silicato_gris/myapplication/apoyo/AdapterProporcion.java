50f9e5fc85eb24256349fc4d29a1c3656c4c5b73
chechis <jcalvarezg92@gmail.com>
2019-06-04 13:06:50 -0600

    adapter fin

--- app/src/main/java/silicato_gris/myapplication/apoyo/AdapterProporcion.java
+++ app/src/main/java/silicato_gris/myapplication/apoyo/AdapterProporcion.java
@@ -2,6 +2,7 @@ package silicato_gris.myapplication.apoyo;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -38,29 +39,56 @@ public class AdapterProporcion extends RecyclerView.Adapter<AdapterProporcion.Co
             txtPropVol = (TextView) itemView.findViewById(R.id.txt_proporcion_volumetrica);
 
             btnEditar = (ImageButton) itemView.findViewById(R.id.btn_proporcion_edit);
-            btnEditar.setOnClickListener();
+            btnEditar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (proporcionListener != null){
+                        proporcionListener.editProporcion(getAdapterPosition());
+                    }
+                }
+            });
+            btnBorrar = (ImageButton) itemView.findViewById(R.id.btn_proporcion_delete);
+            btnBorrar.setOnClickListener(this);
 
         }
 
         @Override
         public void onClick(View view) {
-
+            if (proporcionListener != null){
+                proporcionListener.deleteProporcion(getAdapterPosition());
+            }
         }
     }
 
     @NonNull
     @Override
-    public AdapterProporcion.ConcretoHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public ConcretoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.plantilla_proporcion, parent, false);
+
+        return new ConcretoHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull AdapterProporcion.ConcretoHolder concretoHolder, int i) {
+    public void onBindViewHolder(@NonNull AdapterProporcion.ConcretoHolder holder, int position) {
+        Concreto concreto = concretos.get(position);
 
+        holder.txtAguaCemento.setText("A/C   "+ concreto.getAguaCemento());
+        holder.txtAsentamiento.setText("Asentamiento    "+ concreto.getAsentamiento());
+        holder.txtPropUnitaria.setText("Proporción Unitaria kg/m3    "+concreto.getPropUnitaria());
+        holder.txtPropVol.setText("Proporción Volumetrica    "+concreto.getPropVol());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return concretos.size();
+    }
+
+    public ProporcionListener getProporcionListener() {
+        return proporcionListener;
+    }
+
+    public void setProporcionListener(ProporcionListener proporcionListener) {
+        this.proporcionListener = proporcionListener;
     }
 }
--- app/src/main/java/silicato_gris/myapplication/apoyo/Concreto.java
+++ app/src/main/java/silicato_gris/myapplication/apoyo/Concreto.java
@@ -7,38 +7,51 @@ public class Concreto {
     private int factor;
     private int elemento;
     private int tmn;
-    private int pesoConcrto;
+    private int pesoConcreto;
     private int pesoSueltoFino;
     private int pesoCompactadoFino;
     private int pesoSueltoGrueso;
     private int pesoCompactadoGrueso;
+    private int aguaCemento;
+    private int asentamiento;
+    private int propUnitaria;
+    private int propVol;
+
 
     public Concreto() {
     }
 
-    public Concreto(int id, int resistencia, int factor, int elemento, int tmn, int pesoConcrto, int pesoSueltoFino, int pesoCompactadoFino, int pesoSueltoGrueso, int pesoCompactadoGrueso) {
+    public Concreto(int id, int resistencia, int factor, int elemento, int tmn, int pesoConcreto, int pesoSueltoFino, int pesoCompactadoFino, int pesoSueltoGrueso, int pesoCompactadoGrueso, int aguaCemento, int asentamiento, int propUnitaria, int propVol) {
         this.id = id;
         this.resistencia = resistencia;
         this.factor = factor;
         this.elemento = elemento;
         this.tmn = tmn;
-        this.pesoConcrto = pesoConcrto;
+        this.pesoConcreto = pesoConcreto;
         this.pesoSueltoFino = pesoSueltoFino;
         this.pesoCompactadoFino = pesoCompactadoFino;
         this.pesoSueltoGrueso = pesoSueltoGrueso;
         this.pesoCompactadoGrueso = pesoCompactadoGrueso;
+        this.aguaCemento = aguaCemento;
+        this.asentamiento = asentamiento;
+        this.propUnitaria = propUnitaria;
+        this.propVol = propVol;
     }
 
-    public Concreto(int resistencia, int factor, int elemento, int tmn, int pesoConcrto, int pesoSueltoFino, int pesoCompactadoFino, int pesoSueltoGrueso, int pesoCompactadoGrueso) {
+    public Concreto(int resistencia, int factor, int elemento, int tmn, int pesoConcreto, int pesoSueltoFino, int pesoCompactadoFino, int pesoSueltoGrueso, int pesoCompactadoGrueso, int aguaCemento, int asentamiento, int propUnitaria, int propVol) {
         this.resistencia = resistencia;
         this.factor = factor;
         this.elemento = elemento;
         this.tmn = tmn;
-        this.pesoConcrto = pesoConcrto;
+        this.pesoConcreto = pesoConcreto;
         this.pesoSueltoFino = pesoSueltoFino;
         this.pesoCompactadoFino = pesoCompactadoFino;
         this.pesoSueltoGrueso = pesoSueltoGrueso;
         this.pesoCompactadoGrueso = pesoCompactadoGrueso;
+        this.aguaCemento = aguaCemento;
+        this.asentamiento = asentamiento;
+        this.propUnitaria = propUnitaria;
+        this.propVol = propVol;
     }
 
     public int getId() {
@@ -81,12 +94,12 @@ public class Concreto {
         this.tmn = tmn;
     }
 
-    public int getPesoConcrto() {
-        return pesoConcrto;
+    public int getPesoConcreto() {
+        return pesoConcreto;
     }
 
-    public void setPesoConcrto(int pesoConcrto) {
-        this.pesoConcrto = pesoConcrto;
+    public void setPesoConcreto(int pesoConcreto) {
+        this.pesoConcreto = pesoConcreto;
     }
 
     public int getPesoSueltoFino() {
@@ -119,5 +132,37 @@ public class Concreto {
 
     public void setPesoCompactadoGrueso(int pesoCompactadoGrueso) {
         this.pesoCompactadoGrueso = pesoCompactadoGrueso;
+    }
+
+    public int getAguaCemento() {
+        return aguaCemento;
+    }
+
+    public void setAguaCemento(int aguaCemento) {
+        this.aguaCemento = aguaCemento;
+    }
+
+    public int getAsentamiento() {
+        return asentamiento;
+    }
+
+    public void setAsentamiento(int asentamiento) {
+        this.asentamiento = asentamiento;
+    }
+
+    public int getPropUnitaria() {
+        return propUnitaria;
+    }
+
+    public void setPropUnitaria(int propUnitaria) {
+        this.propUnitaria = propUnitaria;
+    }
+
+    public int getPropVol() {
+        return propVol;
+    }
+
+    public void setPropVol(int propVol) {
+        this.propVol = propVol;
     }
 }
